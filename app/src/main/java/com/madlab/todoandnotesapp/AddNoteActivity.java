7795857971a899820878135bf97c3ed481b1b39f
Aayush Srivastava <aayu.2312@gmail.com>
2020-07-27 22:20:15 +0530

    Remove unused imports

--- app/src/main/java/com/madlab/todoandnotesapp/AddNoteActivity.java
+++ app/src/main/java/com/madlab/todoandnotesapp/AddNoteActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.madlab.todoandnotesapp.data.note.Note;
 
